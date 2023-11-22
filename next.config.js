652e7ed5da9271477850c8e39d3c6c7b59af62b2
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2023-11-23 01:12:25 +0530

    hashnode blog rewrites added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://starter-kit-xi.vercel.app/blog",
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://starter-kit-xi.vercel.app/blog/:path*",
+      },
+    ];
+  }
+}
+
 
 module.exports = nextConfig
+
