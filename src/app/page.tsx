95949f43d5512b4b261effba3d97a76af7ba97e3
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2023-12-14 09:23:25 +0530

    newsletter added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
+import Newsletter from "@/components/Newsletter"
 const page = () => {
   return (
    <>
@@ -15,7 +16,11 @@ const page = () => {
       </div>
     </div>
   </div>
+  
 </section>
+<div>
+  <Newsletter />
+</div>
    </>
   )
 }
---
+++ src/components/Newsletter.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Newsletter = () => {
+  return (
+    <div className=' p-8 bg-indigo-600 flex justify-center items-center'>
+      <iframe 
+        className='block ml-auto mr-auto max-w-full scroll-auto'
+        width="540" 
+        height="540" 
+        allowFullScreen
+        src="https://3885edd1.sibforms.com/serve/MUIFAHlCUNkPpvY6Mzq8WpLjwndqJEowQOh45btHqnze74td8AFASwm6nnlxCuRzB1R_GwgcKA4zzB9K069X6w1d9PqR-hxzJ8kPvDy_GaZceDh9lkmAhzG2FJiQj1bY6IX2A_xZcuLm-BJpTBw9YaQ1DFJ2AlywNqoKfbxQWsjiUVNh1z5NeUVA31NLmtT4l7K-RTChLHdZ8cQ2" 
+        >
+      </iframe>
+    </div>
+  )
+}
+
+export default Newsletter
