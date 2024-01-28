bf97c53b75a4b48623e4a605eb1b15ff89b7a79d
S Soumyakanta <92138792+s-soumyakanta@users.noreply.github.com>
2024-01-28 21:49:51 +0530

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import ContactForm from "@/components/ContactForm";
 const page = () => {
   return (
     <>
-      <div className="dark:bg-dm-bg flex flex-col px-16 space-y-8 w-full justify-center items-center min-h-screen ">
+      <div className="dark:bg-dm-bg flex flex-col px-16 space-y-8 w-full justify-center items-center h-[80vh] ">
         <h2 className="text-lg uppercase dark:text-dm-heading text-lm-heading font-bold">Contact</h2>
         <ContactForm />
       </div>
