2a832fcdb4b88d781412a87fcc5438ce7e67a42a
S Soumyakanta <ssoumyakanta@fightright.in>
2024-07-04 07:43:12 +0530

    cleaned for v4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
-import {LandingPage,About,Work} from '../components/index'
+
 const page = () => {
   return (
-    <>
-      <div className="flex flex-col justify-center items-center ">
-        <div className=" w-full">
-          <section className="min-h-screen w-full flex justify-center items-center flex-col">
-            <LandingPage />
-            <About />
-            <Work />
-          </section>
-        </div>
-      </div>
-    </>
-  );
-};
+    <div>S Soumyakanta</div>
+  )
+}
 
-export default page;
+export default page
