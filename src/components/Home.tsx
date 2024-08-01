451baeb7914ccae6a0c4d0f144e0f47e4e54a4c9
S Soumyakanta <ssoumyakanta@fightright.in>
2024-08-01 09:02:10 +0530

    minor alignment and profile image

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,15 +2,17 @@ import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import About from './About';
 import Works from './Works';
-
+import Image from 'next/image'
+ 
 const Home = () => {
   return (
     <div className="bg-background text-foreground">
       <main className="container mx-auto px-4 py-8 md:py-16">
+        
         <section className="text-center mb-8 h-[80vh] flex flex-col justify-center items-center md:mb-16">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-2 md:mb-4">S Soumyakanta</h1>
           <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-muted-foreground mb-4 md:mb-8">Full-Stack Developer</h2>
-          <p className="max-w-xl md:max-w-lg mx-auto text-base sm:text-lg mb-6 md:mb-8">
+          <p className="max-w-sm md:max-w-lg mx-auto text-base sm:text-lg mb-6 md:mb-8">
             Helping businesses build robust and scalable web applications.
           </p>
           <Link href="/contact">
