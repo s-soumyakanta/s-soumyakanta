c9550ee4862a675dcc412ae01f38538058ec26a1
S Soumyakanta <ssoumyakanta@fightright.in>
2024-08-03 01:15:37 +0530

    defalt dark mode and fonts changed

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -135,13 +135,13 @@ const LandingPage = () => {
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-background text-foreground [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
           <div className="relative z-10 flex flex-col justify-center items-center">
-            <h1 className="text-4xl md:text-6xl lg:text-6xl font-bold mb-2 md:mb-4">
+            <h1 className="text-4xl md:text-6xl font-bold mb-2 md:mb-4">
               S Soumyakanta
             </h1>
             <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-muted-foreground mb-4 md:mb-8">
               Full-Stack Developer
             </h2>
-            <p className="max-w-sm md:max-w-lg mx-auto text-base sm:text-lg mb-6 md:mb-8">
+            <p className="max-w-xs md:max-w-md mx-auto text-base sm:text-lg mb-6 md:mb-8">
               Helping businesses build robust and scalable web applications.
             </p>
             <Link href="/contact">
