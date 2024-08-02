81a8a7f8a7c7c36584ef1286d8dc4cf509c9dd4d
S Soumyakanta <ssoumyakanta@fightright.in>
2024-08-03 00:14:36 +0530

    redundant component removed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,55 +1,28 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import About from "./About";
-import Works from "./Works";
-import Image from "next/image";
-
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import About from './About';
+import Works from './Works';
+import Image from 'next/image'
+ 
 const Home = () => {
-  function DotBackgroundDemo() {
-    return (
-      <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
-        {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-          Backgrounds
-        </p>
-      </div>
-    );
-  }
-
   return (
     <div className="bg-background text-foreground">
       <main className="container mx-auto px-4 py-8 md:py-16">
-        <section className="relative h-[90vh] w-full bg-background text-foreground dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex items-center justify-center text-center mb-8 md:mb-16">
-          {/* Radial gradient for the container to give a faded look */}
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-background text-foreground [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-
-          <div className="relative z-10 flex flex-col justify-center items-center">
-            <h1 className="text-4xl md:text-6xl lg:text-6xl font-bold mb-2 md:mb-4">
-              S Soumyakanta
-            </h1>
-            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-muted-foreground mb-4 md:mb-8">
-              Full-Stack Developer
-            </h2>
-            <p className="max-w-sm md:max-w-lg mx-auto text-base sm:text-lg mb-6 md:mb-8">
-              Helping businesses build robust and scalable web applications.
-            </p>
-            <Link href="/contact">
-              <Button className="uppercase font-bold">Contact</Button>
-            </Link>
-          </div>
+        
+        <section className="text-center mb-8 h-[80vh] flex flex-col justify-center items-center md:mb-16">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-2 md:mb-4">S Soumyakanta</h1>
+          <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-muted-foreground mb-4 md:mb-8">Full-Stack Developer</h2>
+          <p className="max-w-sm md:max-w-lg mx-auto text-base sm:text-lg mb-6 md:mb-8">
+            Helping businesses build robust and scalable web applications.
+          </p>
+          <Link href="/contact">
+            <Button className='uppercase font-bold'>Contact</Button>
+          </Link>
         </section>
-
-        <section
-          id="about"
-          className="min-h-screen flex justify-center items-center"
-        >
+        <section id="about" className='min-h-screen flex justify-center items-center'>
           <About />
         </section>
-        <section
-          id="works"
-          className="min-h-screen flex justify-center items-center"
-        >
+        <section id="works" className='min-h-screen flex justify-center items-center'>
           <Works />
         </section>
       </main>
