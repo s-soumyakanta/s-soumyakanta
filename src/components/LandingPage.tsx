6d059ccca8937784602321cccb97a28dc9daeb27
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2024-08-09 13:44:23 +0530

    Update LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
@@ -7,20 +8,19 @@ import Works from "./Works";
 import Image from "next/image";
 import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/lib/utils";
-import { Spotlight } from "./ui/spotlight";
+//import { Spotlight } from "./ui/spotlight";
 
 const LandingPage = () => {
   return (
     <>
         <section className="relative min-h-screen  w-full flex items-center justify-center text-center mb-8 md:mb-16">
       <div className="min-h-screen w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-center">
-          <Spotlight
+        {/*  <Spotlight
             className="-top-10 left-14 md:left-60 md:-top-20"
             fill="white"
-          />
+          />*/}
           {/* Radial gradient for the container to give a faded look */}
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-
+         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <div className="relative z-10 flex flex-col justify-center items-center">
             <h1 className="text-4xl md:text-6xl font-bold mb-2 md:mb-4">
               S Soumyakanta
