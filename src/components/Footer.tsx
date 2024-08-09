6459288843a5e9e307edc477ff3003daad74e8c7
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2024-08-09 08:34:34 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -78,7 +78,7 @@ export default function Footer() {
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    Twitter
+                    X
                   </a>
                 </li>
               </ul>
