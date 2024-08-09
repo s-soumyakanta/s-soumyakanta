53b074d7a1e2dfd8902c9d73feb4a12a8b4d9097
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2024-08-09 10:32:44 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -88,7 +88,7 @@ export default function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            &copy; {currentYear} S Soumyakanta. All rights reserved.
+            &copy; {currentYear} @s-soumyakanta - All rights reserved.
           </span>
           <div className="flex mt-4 sm:mt-0 sm:justify-center">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
