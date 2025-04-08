9ef81fc09102f087f5dc7367bc0470d675a6b92d
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2025-04-08 15:34:18 +0530

    add removal of the blog robots txt and add in root

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,24 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/',
-        },
-        sitemap: 'https://s-soumyakanta.com/sitemap.xml',
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: '/private/',
+            },
+            {
+                userAgent: 'AdsBot-Google',
+                allow: '/',
+            },
+            {
+                userAgent: 'GPTBot',
+                disallow: '/',
+            },
+        ],
+        sitemap: [
+            'https://s-soumyakanta.com/sitemap.xml',
+            'https://s-soumyakanta.com/blog/sitemap.xml',
+        ],
     };
 }
