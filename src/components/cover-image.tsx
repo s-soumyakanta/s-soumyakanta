5c1d09a6ad0c9566dd89dc2559e6ede4afd65dbf
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2025-04-08 21:18:06 +0530

    add lcp fix for article cover

--- src/components/cover-image.tsx
+++ src/components/cover-image.tsx
@@ -6,23 +6,37 @@ type Props = {
 	src: string;
 	slug?: string;
 	priority?: boolean;
+	width?: number;
+	height?: number;
 };
 
-export const CoverImage = ({ title, src, slug, priority = false }: Props) => {
+export const CoverImage = ({
+	title,
+	src,
+	slug,
+	priority = false,
+	width = 1600,
+	height = 840,
+}: Props) => {
 	const postURL = `/blog/${slug}`;
 
 	const image = (
-		<div className="relative pt-[52.5%]">
+		<div className="relative">
 			<Image
 				src={src}
 				alt={`Cover Image for ${title}`}
 				className="w-full rounded-md border object-cover hover:opacity-90 dark:border-neutral-800"
-				fill
-				unoptimized
+				width={width}
+				height={height}
+				sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+				placeholder="blur"
+				blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8/+F9PQAI+wN4B5X7jwAAAABJRU5ErkJggg=="
 				priority={priority}
+				fetchPriority={priority ? 'high' : 'auto'}
 			/>
 		</div>
 	);
+
 	return (
 		<div className="sm:mx-0">
 			{slug ? (
