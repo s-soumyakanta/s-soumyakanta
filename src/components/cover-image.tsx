aac1de07f6f21ea54dc48e0722e167e64f1fc6f5
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2025-04-09 14:37:58 +0530

    add sckeleton ui for blog images

--- src/components/cover-image.tsx
+++ src/components/cover-image.tsx
@@ -1,42 +1,41 @@
+// cover-image.tsx
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 import Link from 'next/link';
 
-// Define the Props interface
-interface CoverImageProps {
+type CoverImageProps = {
 	title: string;
 	src: string;
 	slug?: string;
 	priority?: boolean;
-}
+};
 
 export const CoverImage = ({ title, src, slug, priority = false }: CoverImageProps) => {
+	const [isLoading, setIsLoading] = useState(true);
 	const postURL = `/blog/${slug}`;
 
-	const image = (
-		<div className="relative pt-[52.5%]">
-			<Image
-				src={src}
-				alt={`Cover Image for ${title}`}
-				className="w-full rounded-md border object-cover hover:opacity-90 dark:border-neutral-800"
-				fill
-				sizes="(max-width: 640px) 100vw, (max-width: 768px) 85vw, (max-width: 1024px) 75vw, 1200px"
-				priority={priority}
-				quality={80}
-				placeholder="blur"
-				blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8/+F9PQAI+wN4B5X7jwAAAABJRU5ErkJggg=="
-			/>
-		</div>
-	);
-
 	return (
-		<div className="sm:mx-0">
-			{slug ? (
-				<Link href={postURL} aria-label={title}>
-					{image}
-				</Link>
-			) : (
-				image
+		<div className="relative w-full">
+			{/* Skeleton UI */}
+			{isLoading && (
+				<div className="absolute inset-0 animate-pulse rounded-md bg-gray-200 dark:bg-neutral-800" />
 			)}
+			<Link href={postURL}>
+				<Image
+					src={src}
+					alt={`Cover Image for ${title}`}
+					width={1600} // Match the resized width
+					height={840} // Match the resized height
+					className={`w-full rounded-md border object-cover hover:opacity-90 dark:border-neutral-800 ${isLoading ? 'opacity-0' : 'opacity-100'
+						}`}
+					sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 1600px" // Responsive sizes
+					priority={priority} // Preload if LCP element
+					onLoad={() => setIsLoading(false)}
+					onError={() => setIsLoading(false)} // Fallback if image fails
+				/>
+			</Link>
 		</div>
 	);
 };
