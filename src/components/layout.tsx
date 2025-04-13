125d34f4f355ae234881b4d89c57768b1a59235b
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2025-04-14 00:32:30 +0530

    add hashnode tracking delete blog meta data updated

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,22 +1,66 @@
-import { Analytics } from './analytics';
-import { Integrations } from './integrations';
-import { Meta } from './meta';
-import { Scripts } from './scripts';
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+	title: "S Soumyakanta's Blog | Fullstack Developer",
+	description: 'Personal blog of S Soumyakanta - MERN & Golang Developer',
+	keywords: 'fullstack, developer, MERN, React, Node.js, MongoDB, Express, Golang, web development',
+	authors: [{ name: 'S Soumyakanta' }],
+	creator: 'S Soumyakanta',
+	openGraph: {
+		title: "S Soumyakanta's Blog | Fullstack Developer",
+		description: 'Personal blog of S Soumyakanta - MERN & Golang Developer',
+		url: 'https://s-soumyakanta.com/blog',
+		siteName: "S Soumyakanta's Blog",
+		locale: 'en_US',
+		type: 'website',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: "S Soumyakanta's Blog | Fullstack Developer",
+		description: 'Personal blog of S Soumyakanta - MERN & Golang Developer',
+		creator: '@s-soumyakanta',
+	},
+	icons: {
+		icon: [
+			{ url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+			{ url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+		],
+		apple: { url: '/favicon/apple-touch-icon.png', sizes: '180x180' },
+		other: [
+			{
+				rel: 'mask-icon',
+				url: '/favicon/safari-pinned-tab.svg',
+				color: '#000000'
+			},
+		],
+	},
+	themeColor: '#000000',
+	applicationName: "S Soumyakanta's Blog",
+	metadataBase: new URL('https://s-soumyakanta.com/blog'),
+	alternates: {
+		types: {
+			'application/rss+xml': '/feed.xml',
+		},
+	},
+	other: {
+		'msapplication-TileColor': '#000000',
+		'msapplication-config': '/favicon/browserconfig.xml',
+	},
+};
 
 type Props = {
 	children: React.ReactNode;
 };
 
-export const Layout = ({ children }: Props) => {
+export default function RootLayout({ children }: Props) {
 	return (
-		<>
-			<Meta />
-			<Scripts />
-			<div className="min-h-screen bg-white dark:bg-neutral-950">
-				<main>{children}</main>
-			</div>
-			<Analytics />
-			<Integrations />
-		</>
+		<html lang="en">
+			<body>
+				<div className="min-h-screen bg-white dark:bg-neutral-950">
+					<main>{children}</main>
+				</div>
+			</body>
+		</html>
 	);
-};
+}
